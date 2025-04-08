9b7f7537d8f8f3c3e8dd08bbb66ec7c1d907cdcf
Blazej Bezak <125065137+cmikrow@users.noreply.github.com>
2025-04-08 21:16:51 +0200

    Delete Zad3.py

--- Zad3.py
+++
@@ -1,7 +0,0 @@
-ulice = ["Jagodowa","Lipowa","Kiwatowa","Kasztanowa","Polna"]
-bloki = [1,2,3,4,5]
-lokale = [1,2,3,4,5,6,7,8,9,10]
-for ulica in ulice:
-    for blok in bloki:
-        for lokal in lokale:
-            print(f"{ulica} {blok} blok , {lokal} lokal")
